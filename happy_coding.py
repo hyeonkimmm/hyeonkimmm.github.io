6077c54db5b2d8f8c9415c92faca036c15fca5cf
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2021-07-27 23:36:28 +0900

    create

--- happy_coding.py
+++ happy_coding.py
@@ -1,141 +1,37 @@
-def solution(rows, columns, queries):
+def cal_dividend(seller,amount,member_dict={}):
+    dividend = int(amount*0.1)
+    # 배당금 10% 1미만
+    if dividend < 1 :
+        member_dict[seller][1] += amount # 그대로 순이익
+    # 배당금 10% 1이상 and 추천인 X
+    elif member_dict[seller][0] =='-':
+        member_dict[seller][1] += amount - dividend
+    # 배당금 10% 1이상 and 추천인 O
+    else:
+        member_dict[seller][1] += amount - dividend
+        cal_dividend(member_dict[seller][0],dividend,member_dict)
+
+def solution(enroll, referral, seller, amount):
+    member_dict = {}
+    for en,re in zip(enroll,referral):
+        member_dict[en]=[re,0]
+    amount = list(map(lambda x:x*100,amount))
+    for sellery,count in zip(seller,amount):
+        cal_dividend(sellery, count,member_dict)
     answer = []
-    matrix = [[i*columns+j+1 for j in range(columns)]for i in range(rows)]
-    for query in queries:
-        min_num = rows*columns
-        x_move,y_move = query[2]-query[0],query[3]-query[1]
-        # 아래로 x_move 만큼 이동
-        x,y = query[0]-1,query[1]-1
-        for _ in range(1,x_move+1):
-            # 현재 위치 저장
-            if matrix[x+1][y] <= min_num:
-                min_num = matrix[x+1][y]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x+1][y]
-            matrix[x+1][y] = temp
-            x += 1 # 자리 갱신
-        # 오른쪽으로 y_move만큼 이동
-        for _ in range(1,y_move+1):
-            # 현재 위치 저장
-            if matrix[x][y+1] <= min_num:
-                min_num = matrix[x][y+1]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x][y+1]
-            matrix[x][y+1] = temp
-            y += 1 # 자리 갱신
-        # 위로 X_move 만큼 이동
-        for _ in range(1,x_move+1):
-            # 현재 위치 저장
-            if matrix[x-1][y] <= min_num:
-                min_num = matrix[x-1][y]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x-1][y]
-            matrix[x-1][y] = temp
-            x -= 1 # 자리 갱신
-        # 왼쪽으로 y_move-1 만큼 이동
-        for _ in range(1,y_move):
-            # 현재 위치 저장
-            if matrix[x][y-1] <= min_num:
-                min_num = matrix[x][y-1]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x][y-1]
-            matrix[x][y-1] = temp
-            y -= 1 # 자리 갱신
-        answer.append(min_num)
-    return answer
-def solution(rows, columns, queries):
-    answer = []
-    matrix = [[i*columns+j+1 for j in range(columns)]for i in range(rows)]
-    for query in queries:
-        min_num = rows*columns
-        x_move,y_move = query[2]-query[0],query[3]-query[1]
-        # 아래로 x_move 만큼 이동
-        x,y = query[0]-1,query[1]-1
-        for _ in range(1,x_move+1):
-            # 현재 위치 저장
-            if matrix[x+1][y] <= min_num:
-                min_num = matrix[x+1][y]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x+1][y]
-            matrix[x+1][y] = temp
-            x += 1 # 자리 갱신
-        # 오른쪽으로 y_move만큼 이동
-        for _ in range(1,y_move+1):
-            # 현재 위치 저장
-            if matrix[x][y+1] <= min_num:
-                min_num = matrix[x][y+1]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x][y+1]
-            matrix[x][y+1] = temp
-            y += 1 # 자리 갱신
-        # 위로 X_move 만큼 이동
-        for _ in range(1,x_move+1):
-            # 현재 위치 저장
-            if matrix[x-1][y] <= min_num:
-                min_num = matrix[x-1][y]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x-1][y]
-            matrix[x-1][y] = temp
-            x -= 1 # 자리 갱신
-        # 왼쪽으로 y_move-1 만큼 이동
-        for _ in range(1,y_move):
-            # 현재 위치 저장
-            if matrix[x][y-1] <= min_num:
-                min_num = matrix[x][y-1]
-            if matrix[x][y] <= min_num:
-                min_num = matrix[x][y]
-            temp = matrix[x][y]
-            # 현재 위치에 넣어줌
-            matrix[x][y] = matrix[x][y-1]
-            matrix[x][y-1] = temp
-            y -= 1 # 자리 갱신
-        answer.append(min_num)
+    for member in enroll:
+        answer.append(member_dict[member][1])
     return answer
 
-# rows = 6
-# columns =6 
-# queries = [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]
+enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
+referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
 
-# rows = 3
-# columns = 3
-# queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]] 
+# seller = ["young", "john", "tod", "emily", "mary"]
+# amount = [12, 4, 2, 5, 10]
+# result = [360, 958, 108, 0, 450, 18, 180, 1080]
 
-rows = 100
-columns = 97
-queries = [[1,1,100,97]] 
+seller = ["sam", "emily", "jaimie", "edward"]
+amount = [2, 3, 5, 4]
+result = [0, 110, 378, 180, 270, 450, 0, 0]
 
-print(solution(rows,columns,queries)) 
-# rows = 6
-# columns =6 
-# queries = [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]
-
-# rows = 3
-# columns = 3
-# queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]] 
-
-rows = 100
-columns = 97
-queries = [[1,1,100,97]] 
-
-print(solution(rows,columns,queries)) 
+print(solution(enroll,referral,seller,amount))
